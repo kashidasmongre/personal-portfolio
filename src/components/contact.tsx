3ad0c66136cc6d656b6b89e6698bdbff7ad68356
adwelink <adwelink@gmail.com>
2025-09-26 02:46:20 +0000

    enme  "+91 830505008 , +91 8305105008, kashidasmongre.x@gmail .com " add

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -50,12 +50,12 @@ const Contact = () => {
         >
             <div className="flex flex-col sm:flex-row justify-center gap-4">
               <Button asChild size="lg" className="w-full sm:w-auto">
-                <Link href="tel:+911234567890">
+                <Link href="tel:+91830505008">
                   <Phone className="mr-2 h-4 w-4" /> Call Me
                 </Link>
               </Button>
                <Button asChild size="lg" className="w-full sm:w-auto bg-green-500 hover:bg-green-600">
-                <Link href="https://wa.me/911234567890" target="_blank">
+                <Link href="https://wa.me/918305105008" target="_blank">
                   <MessageCircle className="mr-2 h-4 w-4" /> WhatsApp
                 </Link>
               </Button>
